3917e45522c7f09072b305894ba351082fb44ea5
Venkata Sanjeeva Kuntumalla <kuntumallavenkatasanjeeva2005@gmail.com>
2024-11-17 12:36:59 +0530

    added cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 
 require("dotenv").config()
 
+const cors = require("cors")
 const router = require("./routes/APTRoute")
 
 const connectDB = require("./db/connect")
@@ -15,6 +16,8 @@ const app = express();
 // app.use(express.json());
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/", router)
 
